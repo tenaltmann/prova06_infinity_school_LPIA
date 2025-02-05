c1cc9e8619407880145bbe222c93581a3a793adf
Ricardo Altmann <tenaltmann@gmail.com>
2025-02-05 15:10:59 -0300

    Resolução da prova 06 do modulo LPIA Infinity School

---
+++ README.md
@@ -0,0 +1,8 @@
+Resolução da Prova do aluno: Ricardo Soares de Moura Altmann Filho 20/10/2002 LPIA | Lógica da Programação com ChatGPT
+
+Enunciado:  Crie um programa em Python que simule um sistema de login. O programa deve permitir ao usuário três tentativas para acertar o nome de usuário e a senha corretos. Caso o usuário erre as credenciais, o programa deve fornecer uma mensagem informando quantas tentativas restam. Se o usuário acertar, uma mensagem de boas-vindas deve ser exibida, e o programa deve terminar imediatamente.
+
+Se todas as três tentativas falharem, o programa deve usar um loop for para exibir uma mensagem de "Acesso bloqueado" repetida três vezes.
+
+
+OBS: caso queira esconder os inputs de senha com ****, basta descomentar o "import pwinput" e as variaveis comentadas por suas versoes não comentadas.
--- prova06.py
+++ prova06.py
@@ -1,18 +1,46 @@
-import pwinput
-nome_usuario = str(input("Defina seu nome de usuário"))
-senha = pwinput("Defina sua senha:")
-tentivas = 3
+
+# caso queira esconder os inputs de senha com ****, basta descomentar o "import pwinput" e as variaveis comentadas por suas versoes não comentadas.
+ 
+#import pwinput                                                                 
 
 
-while True:
+nome_usuario = str(input("Defina seu nome de usuário: "))                       #define o nome de usuário
+senha = str(input("Defina sua senha: "))                                        #define a senha
+#senha = pwinput.pwinput("Defina sua senha: ")  
+tentativas = 3                                                                  #contador de tentativas
 
-    print(" 1. Efetuar o login 2. Sair")
-    opcao = int(input("Escolha uma opção: "))
 
-    if opcao == 1:
-        for i in range(tentivas):
-            confere_usuario = str(input("Usuário: "))
-            confere_senha = str(input("Senha: "))
-            if confere_usuario == nome_usuario and confere_senha == senha == True:
-                print("Login Efetuado")
+while tentativas > 0:                                                           #estrutura de repetição que permite a visualização do menu a cada tentativa
 
+    
+    print(" 1. Efetuar o login 2. Sair")                        
+    opcao = int(input("Escolha uma opção: "))                                   # seleciona a opçãp do menu
+
+    if opcao == 1:                                                              # condicional de realização do login
+        for i in range(tentativas):                                             # Loop de repetição limitada ao número de tentativas
+
+            confere_usuario = str(input("Usuário: "))                           #variavel do nome de usuário no login
+            confere_senha = str(input("Senha: "))                               #variavel da senha no login
+            #confere_senha = pwinput.pwinput("Senha: ")
+
+            if confere_usuario == nome_usuario and confere_senha == senha:      #condicional que confere se todas as credenciais conferem
+                print("Boas Vindas, seu Login foi efetuado com sucesso")        
+                tentativas = 0                                                  #zera as tentivas encerrando o loop while e for
+                break                                                           
+            else:
+                tentativas -= 1                                                 # diminui a tentativa e menos 1
+                if tentativas >= 1:                                             # condicional que mostra o número de tentativas restante
+                    print(f"Você possui mais {tentativas} tentativa(s)")
+                else:       
+                    for i in range(3):                                          # loop de repetição das 3 mensagens de acesso bloqueado conforme solicitado no enunciado
+                        print("Acesso Bloqueado !")
+                    break
+
+    elif opcao == 2:                                                            # opção de encerrar o código
+        print("Saindo...")
+        break
+    else:                                                                       # força o usuário a responder conforme o menu
+        print("Escolha uma opção valida!")
+
+
+  
